181dd2ee9e68e254f4c03beba769d790b5905f57
Mercy Mbungu <mmbungu@byupathway.edu>
2026-07-05 18:36:57 +0100

    Add Google font usage

--- scripts/course.js
+++ scripts/course.js
@@ -9,7 +9,7 @@ const courses = [
         technology: [
             'Python'
         ],
-        completed: false
+        completed: true
     },
     {
         subject: 'WDD',
@@ -22,7 +22,7 @@ const courses = [
             'HTML',
             'CSS'
         ],
-        completed: false
+        completed: true
     },
     {
         subject: 'CSE',
@@ -74,7 +74,7 @@ const courses = [
             'CSS',
             'JavaScript'
         ],
-        completed: true
+        completed: false
     }
 ]
 
